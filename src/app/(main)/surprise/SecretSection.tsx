e41ed939d1f0264d79d354031e0166e56ec3bc4f
Greg <mistzero0@gmail.com>
2023-04-22 18:01:05 +0800

    add diff button routing to secret page

--- src/app/(main)/surprise/SecretSection.tsx
+++ src/app/(main)/surprise/SecretSection.tsx
@@ -1,10 +1,17 @@
 "use client";
+import Link from "next/link";
 import { useRouter } from "next/navigation";
 import React, { useState } from "react";
 
 const ModalSection = () => {
 	const router = useRouter();
-	return <button onClick={() => router.push("/secret")}>Click me</button>;
+
+	return (
+		<div className="flex flex-col">
+			<Link href="/secret">Server side</Link>
+			<button onClick={() => router.push("/secret")}>Client side</button>
+		</div>
+	);
 };
 
 export default ModalSection;
